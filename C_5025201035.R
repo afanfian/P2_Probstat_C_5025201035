8b56ecc49058833629e2253e7772c8cbabc4591e
afanfian <afanfian14@gmail.com>
2022-05-29 18:53:47 +0700

    Update soal 1 - 5

--- C_5025201035.R
+++ C_5025201035.R
@@ -36,7 +36,47 @@ qchisq(p = 0.05, df = 2, lower.tail=FALSE)
 
 
 # Soal 4
+# A
+dataoneway <- read.table("onewayanova.txt",h=T)
+attach(dataoneway)
+names(dataoneway)
 
+dataoneway$Group <- as.factor(dataoneway$Group)
+dataoneway$Group = factor(dataoneway$Group,labels = c("Kucing Oren", "Kucing Hitam", "Kucing Putih"))
+
+class(dataoneway$Group)
+
+Group1 <- subset(dataoneway, Group == "Kucing Oren")
+Group2 <- subset(dataoneway, Group == "Kucing Hitam")
+Group3 <- subset(dataoneway, Group == "Kucing Putih")
+
+qqnorm(Group1$Length)
+qqline(Group1$Length)
+
+qqnorm(Group2$Length)
+qqline(Group2$Length)
+
+qqnorm(Group2$Length)
+qqline(Group2$Length)
+
+# B
+bartlett.test(Length ~ Group, data = dataoneway)
+
+# C
+model1 = lm(Length ~ Group, data = dataoneway)
+anova(model1)
+
+# D
+TukeyHSD(aov(model1))
+
+# E & F
+install.packages("ggplot2")
+library("ggplot2")
+
+ggplot(dataoneway, aes(x = Group, y = Length)) +
+  geom_boxplot(fill = "grey80", colour = "black") +
+  scale_x_discrete() + xlab("Treatment Group") +
+  ylab("Length (cm)")
 
 
 # Soal 5
