c522d791e7653cae9d406d8311cc22639320475d
afanfian <afanfian14@gmail.com>
2022-05-29 17:37:41 +0700

    Update code Soal 1 - 5

---
+++ C_5025201035.R
@@ -0,0 +1,82 @@
+# Soal 1
+# A
+x <- c(78,75,67,77,70,72,78,74,77)
+y <- c(100,95,70,90,90,90,89,90,100)
+sd(x-y)
+
+# B
+t.test (y , x , paired=TRUE)
+
+
+# Soal 2
+install.packages("BSDA")
+library(BSDA)
+# B
+zsum.test(mean.x=23500, sigma.x = 3900, n.x = 100,  
+          alternative = "greater", mu = 20000,
+          conf.level = 0.95)
+
+
+# Soal 3
+install.packages("BSDA")
+library(BSDA)
+
+# B 
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
+          mean.y =2.79 , s.y = 1.32, n.y = 27, 
+          alternative = "greater", var.equal = TRUE)
+
+# C
+install.packages("mosaic")
+library(mosaic)
+plotDist(dist='t', df=2, col="blue")
+
+# D
+qchisq(p = 0.05, df = 2, lower.tail=FALSE)
+
+
+# Soal 4
+
+
+
+# Soal 5
+# A
+install.packages("multcompView")
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+
+str(GTL)
+
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+
+# B
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+summary(anova)
+
+# C
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+# D
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+# E
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
+
+cld <- as.data.frame.list(tukey.cld$`Glass:Temp_Factor`)
+data_summary$Tukey <- cld$Letters
+print(data_summary)
